f91f72f76f39030107b45895e526fcb75d3cdcd8
L Prathyusha <83442712+Prathyusha-L@users.noreply.github.com>
2021-08-27 14:02:12 +0530

    Add files via upload

---
+++ array(arthematic operations).R
@@ -0,0 +1,12 @@
+array1=array(c(1:10),dim=c(3,3,2))
+v1=c(11:20)
+array2=array(v1,dim=c(3,3,2))
+print(array1)
+print(array2)
+
+
+print(array1+array2)
+print(array1-array2)
+print(array1*array2)
+print(array1/array2)
+print(array1^array2)
